a0da7ae4f52c72fa1ccb79cef9de712f4dce42d2
jackchealdang <jackchealdang@gmail.com>
2024-01-30 00:36:36 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <body>
+      <main>
+      </main>
+      <footer>
+<div class="attribution">
+    Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+    Coded by <a href="#">Your Name Here</a>.
+  </div>
+      </footer>
+      </body>
+    </>
+  )
+}
+
+export default App
